edfe49f12c767aad40fd0661621b806aa7348fb4
Alec Iverson <adiverso93@gmail.com>
2015-11-14 02:50:52 -0700

    Fixed issue #2
    That was embarrassing.

--- Script.hpp
+++ Script.hpp
@@ -19,14 +19,14 @@ class Script
 		// given the name of a function in the Lua script, will call it
 		// returns true if running was succesful, false otherwise
 		template<typename... Args>
-		bool call(const std::string& function, Args&&... args);
+		void call(const std::string& function, Args&&... args);
 		
 	private:
 		lna::State luaState;
 };
 
 template<typename... Args>
-bool Script::call(const std::string& function, Args&&... args)
+void Script::call(const std::string& function, Args&&... args)
 {
 	luaState[function](args...);
 	
